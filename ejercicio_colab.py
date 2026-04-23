fad733fc46d638bd5b4dbbcfd0e36362de599244
Manu Lopez <manulopez.zxx@gmail.com>
2026-04-23 15:02:50 -0500

    Agrego clase Cliente

--- ejercicio_colab.py
+++ ejercicio_colab.py
@@ -1,1 +1,9 @@
 #Trabajo colaborativo - fase 4 componente practico
+
+class Cliente: #
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+        
+    def mostrar_informacion(self):
+        print(f"Cliente {self.id}: {self.nombre}") 
